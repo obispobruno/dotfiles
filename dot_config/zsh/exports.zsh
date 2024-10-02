1ab941a65b05e9924fe168c0946e730a460a8680
obispobruno <obispobruno@tuta.com>
2024-10-02 02:18:33 -0300

    Update .config/zsh/exports.zsh

--- dot_config/zsh/exports.zsh
+++ dot_config/zsh/exports.zsh
@@ -1,10 +1,10 @@
 # export FPATH="$FPATH:/home/linuxbrew/.linuxbrew/share/zsh/site-functions"
-export FPATH="$FPATH:/usr/share/zsh/vendor-completions"
-export PATH="$PATH:/home/bruno/.local/bin"
-export PATH="$PATH:/home/bruno/.cargo/bin"
+export FPATH="$FPATH:/var/usr/share/zsh/vendor-completions"
+export PATH="$PATH:/var/home/bruno/.local/bin"
+export PATH="$PATH:/var/home/bruno/.cargo/bin"
 export PATH=$HOME/bin:/usr/local/bin:$PATH
-export PATH="/home/linuxbrew/.linuxbrew/opt/binutils/bin:$PATH"
-export XDG_DATA_DIRS="/home/linuxbrew/.linuxbrew/share:$XDG_DATA_DIRS"
+export PATH="/var/home/linuxbrew/.linuxbrew/opt/binutils/bin:$PATH"
+export XDG_DATA_DIRS="/var/home/linuxbrew/.linuxbrew/share:$XDG_DATA_DIRS"
 export HOMEBREW_AUTO_UPDATE_SECS=1800
 export EDITOR=nvim
 # export HOMEBREW_TEMP=/var/tmp
