c09fa58a3df3db649c9aa1b599ddb08eb2db2d4d
obispobruno <obispobruno@tuta.com>
2024-10-04 19:43:58 -0300

    Update .config/zsh/exports.zsh

--- dot_config/zsh/exports.zsh
+++ dot_config/zsh/exports.zsh
@@ -3,6 +3,7 @@ export FPATH="$FPATH:/var/usr/share/zsh/vendor-completions"
 export PATH="$PATH:/var/home/bruno/.local/bin"
 export PATH="$PATH:/var/home/bruno/.cargo/bin"
 export PATH="$PATH:/home/bruno/.dotnet/tools"
+export DOTNET_ROOT="/home/linuxbrew/.linuxbrew/opt/dotnet/libexec"
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 export PATH="/var/home/linuxbrew/.linuxbrew/opt/binutils/bin:$PATH"
 export XDG_DATA_DIRS="/var/home/linuxbrew/.linuxbrew/share:$XDG_DATA_DIRS"
