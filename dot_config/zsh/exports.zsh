80b5216be01bf3689eb0d61ee7cf12cb6833ed68
obispobruno <obispobruno@tuta.com>
2024-11-25 20:46:45 -0300

    Update .config/zsh/exports.zsh

--- dot_config/zsh/exports.zsh
+++ dot_config/zsh/exports.zsh
@@ -5,13 +5,14 @@ export PATH="$PATH:/home/$USER/.cargo/bin"
 export PATH="$PATH:/home/$USER/.dotnet/tools"
 export DOTNET_ROOT="/home/linuxbrew/.linuxbrew/opt/dotnet/libexec"
 export PATH="/home/linuxbrew/.linuxbrew/opt/dotnet@8/bin:$PATH"
-export PATH="/home/linuxbrew/.linuxbrew/opt/node@22/bin:$PATH"
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 export PATH="/home/linuxbrew/.linuxbrew/opt/binutils/bin:$PATH"
 export XDG_DATA_DIRS="/home/linuxbrew/.linuxbrew/share:$XDG_DATA_DIRS"
 export HOMEBREW_AUTO_UPDATE_SECS=1800
 export TZ="America/Montevideo"
 export EDITOR=nvim
+export NVM_DIR="$HOME/.nvm"
+
 # export HOMEBREW_TEMP=/var/tmp
 
 function yy() {
