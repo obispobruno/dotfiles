05d3aacec3e2fe1178df1679ca81e8619eb5cbe5
obispobruno <obispobruno@tuta.com>
2024-12-04 00:19:23 -0300

    Update .config/nvim/lua/core/lazy.lua

--- dot_config/nvim/lua/core/lazy.lua
+++ dot_config/nvim/lua/core/lazy.lua
@@ -12,7 +12,19 @@ local plugins = {
   'hrsh7th/cmp-path',
   'L3MON4D3/LuaSnip',
   'simrat39/rust-tools.nvim',
-
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
   'numToStr/Comment.nvim',
   'folke/zen-mode.nvim',
   'ThePrimeagen/harpoon',
