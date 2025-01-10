6b137d736288818fcf2dc7a956e1c12adb57e320
obispobruno <obispobruno@tuta.com>
2025-01-10 11:09:35 -0300

    Remove .config/nvim/after/plugin/fterm.lua

--- dot_config/nvim/after/plugin/fterm.lua
+++
@@ -1,5 +0,0 @@
-local nmap = require('core.keymap').nmap
-local tmap = require('core.keymap').tmap
-
-nmap('<A-i>', '<CMD>lua require("FTerm").toggle()<CR>', 'toggle floating term')
-tmap('<A-i>', '<CMD>lua require("FTerm").toggle()<CR>', 'toggle floating term')
