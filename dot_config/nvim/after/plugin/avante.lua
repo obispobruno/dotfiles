4194bff0e8ee4b85ef2f823149fc07eba11b6bec
obispobruno <obispobruno@tuta.com>
2025-02-19 22:41:37 -0300

    Update .config/nvim/after/plugin/avante.lua

--- dot_config/nvim/after/plugin/avante.lua
+++ dot_config/nvim/after/plugin/avante.lua
@@ -6,10 +6,11 @@ require('avante').setup({
   ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
   provider = 'claude',
   auto_suggestions_provider = 'claude',
+  cursor_applying_provider = 'groq',
   claude = {
     endpoint = 'https://api.anthropic.com',
     model = 'claude-3-5-sonnet-20241022',
-    timeout = 30000, -- Timeout in milliseconds
+    timeout = 30000,
     temperature = 0,
     max_tokens = 8000,
   },
@@ -20,6 +21,13 @@ require('avante').setup({
       endpoint = 'http://localhost:11434/v1',
       model = 'deepseek-r1:8b',
     },
+    groq = {
+      __inherited_from = 'openai',
+      api_key_name = 'GROQ_API_KEY',
+      endpoint = 'https://api.groq.com/openai/v1/',
+      model = 'qwen-2.5-coder-32b',
+      max_tokens = 8192,
+    },
   },
   behaviour = {
     auto_suggestions = false,
@@ -28,6 +36,7 @@ require('avante').setup({
     auto_apply_diff_after_generation = true,
     support_paste_from_clipboard = false,
     minimize_diff = true,
+    enable_cursor_planning_mode = true,
   },
   mappings = {
     --- @class AvanteConflictMappings
