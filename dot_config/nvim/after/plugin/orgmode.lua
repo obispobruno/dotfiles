56431294a550d10ab9f6b2848f4e7bb500c4df55
obispobruno <obispobruno@tuta.com>
2025-03-24 10:44:45 -0300

    Update .config/nvim/after/plugin/orgmode.lua

--- dot_config/nvim/after/plugin/orgmode.lua
+++ dot_config/nvim/after/plugin/orgmode.lua
@@ -1,5 +1,5 @@
 -- Setup orgmode
-require('orgmode').setup({
-  org_agenda_files = '~/Syncthing/orgfiles/**/*',
-  org_default_notes_file = '~/Syncthing/orgfiles/refile.org',
-})
+-- require('orgmode').setup({
+--   org_agenda_files = '~/Syncthing/orgfiles/**/*',
+--   org_default_notes_file = '~/Syncthing/orgfiles/refile.org',
+-- })
