52a388e35c8cf9c36c1eb9bf7a7ae8bc59422bda
obispobruno <obispobruno@tuta.com>
2025-07-22 22:59:14 -0300

    Update .config/nvim/after/plugin/avante.lua

--- dot_config/nvim/after/plugin/avante.lua
+++ dot_config/nvim/after/plugin/avante.lua
@@ -7,6 +7,16 @@ require('avante').setup({
   provider = 'claude',
   auto_suggestions_provider = 'claude',
   cursor_applying_provider = 'groq',
+  system_prompt = function()
+    local hub = require('mcphub').get_hub_instance()
+    return hub and hub:get_active_servers_prompt() or ''
+  end,
+  -- Using function prevents requiring mcphub before it's loaded
+  custom_tools = function()
+    return {
+      require('mcphub.extensions.avante').mcp_tool(),
+    }
+  end,
   providers = {
     copilot = {
       model = 'gpt-4.1',
@@ -44,7 +54,7 @@ require('avante').setup({
   },
   web_search_engine = {
     provider = 'searxng', -- tavily, serpapi, searchapi, google, kagi, brave, or searxng
-    proxy = nil,          -- proxy support, e.g., http://127.0.0.1:7890
+    proxy = nil, -- proxy support, e.g., http://127.0.0.1:7890
   },
   behaviour = {
     auto_suggestions = false,
@@ -91,10 +101,10 @@ require('avante').setup({
   windows = {
     ---@type "right" | "left" | "top" | "bottom"
     position = 'right', -- the position of the sidebar
-    wrap = true,        -- similar to vim.o.wrap
-    width = 30,         -- default % based on available width
+    wrap = true, -- similar to vim.o.wrap
+    width = 30, -- default % based on available width
     sidebar_header = {
-      enabled = true,   -- true, false to enable/disable the header
+      enabled = true, -- true, false to enable/disable the header
       align = 'center', -- left, center, right for title
       rounded = true,
     },
@@ -107,7 +117,7 @@ require('avante').setup({
       start_insert = true, -- Start insert mode when opening the edit window
     },
     ask = {
-      floating = false,     -- Open the 'AvanteAsk' prompt in a floating window
+      floating = false, -- Open the 'AvanteAsk' prompt in a floating window
       start_insert = false, -- Start insert mode when opening the ask window
       border = 'rounded',
       ---@type "ours" | "theirs"
