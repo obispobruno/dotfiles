492a3270e7dcaa1239d6846b8e7c33dd96de1e1a
obispobruno <obispobruno@tuta.com>
2025-09-15 10:59:41 -0300

    Update .config/zsh/opts.zsh

--- dot_config/zsh/opts.zsh
+++ dot_config/zsh/opts.zsh
@@ -11,4 +11,6 @@ setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 setopt share_history          # share command history data
 
+zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
+
 bindkey -v
