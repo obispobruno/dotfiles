695ff828763c2575ac4829b623a74703caf8d56e
obispobruno <obispobruno@tuta.com>
2026-01-20 22:04:27 -0300

    Update .config/nvim/lua/core/lazy.lua

--- dot_config/nvim/lua/core/lazy.lua
+++ dot_config/nvim/lua/core/lazy.lua
@@ -49,9 +49,8 @@ local plugins = {
   },
   {
     'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
+    branch = 'master',
+    lazy = false,
     build = ':TSUpdate',
   },
   {
